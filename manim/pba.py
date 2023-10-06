e8408b391c23688fe94093a1c282afedd30c690d
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-10-07 00:23:04 +0200

    add(save): draft

---
+++ manim/pba.py
@@ -0,0 +1,56 @@
+from manim import *
+
+class OpeningManim(Scene):
+    def construct(self):
+        title = Tex(r"Difficulty formula")
+        basel = MathTex(r"\text{Difficulty}(n) = \text{Difficulty}(n-1) \times \left( \frac{\text{2 weeks}}{\text{mine last 2016 blocks}} \right)")
+
+        VGroup(title, basel).arrange(DOWN)
+        self.play(
+            Write(title),
+            FadeIn(basel, shift=DOWN),
+        )
+        self.wait()
+
+        transform_title = Tex("Polkadot Blockchain Academy")
+        transform_title.to_corner(UP + LEFT)
+        self.play(
+            Transform(title, transform_title),
+            LaggedStart(*[FadeOut(obj, shift=DOWN) for obj in basel]),
+        )
+        self.wait()
+
+        grid = NumberPlane()
+        grid_title = Tex("People and Talent", font_size=72)
+        grid_title.move_to(transform_title)
+
+        self.add(grid, grid_title)  # Make sure title is on top of grid
+        self.play(
+            FadeOut(title),
+            FadeIn(grid_title, shift=UP),
+            Create(grid, run_time=3, lag_ratio=0.1),
+        )
+        self.wait()
+
+        grid_transform_title = Tex(
+            r"Projects, Ideas after \\ Polkadot Blockchain Academy"
+        )
+        self.wait()
+        grid_transform_title.move_to(grid_title, UL)
+        grid.prepare_for_nonlinear_transform()
+        self.play(
+            grid.animate.apply_function(
+                lambda p: p
+                          + np.array(
+                    [
+                        np.sin(p[1]),
+                        np.sin(p[0]),
+                        0,
+                    ]
+                )
+            ),
+            run_time=3,
+        )
+        self.wait()
+        self.play(Transform(grid_title, grid_transform_title))
+        self.wait(5)
